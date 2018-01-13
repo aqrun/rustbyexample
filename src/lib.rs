8da2eeca2e8d108e61761f1c56d16378a260c5aa
Aqrun <aqrun@oicnp.com>
2018-01-13 22:53:38 +0800

    primitives

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 pub mod hello;
+mod primitives;
 
 pub fn module_init(module_name: &str){
     match module_name {
         "hello" => hello::mod_init(),
+        "primitives" => primitives::mod_init(),
         _ => println!("Module ({}) not exists!", module_name),
     }
 }
---
+++ src/primitives/mod.rs
@@ -0,0 +1,20 @@
+// 2. 原生类型
+
+pub fn mod_init() {
+
+    run();
+}
+
+fn run() {
+    let _logical: bool = true;
+
+    let _a_float: f64 = 1.0;
+    let _an_integer = 5i32;
+
+    let _default_float = 3.0;
+    let _default_integer = 7;
+
+    let mut _mutable = 12;
+
+    //mutable = true;
+}
