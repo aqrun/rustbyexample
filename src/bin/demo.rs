9f4dbf8f7bc072c24b5693770a5af19d17ae8d34
Aqrun <aqruna@gmail.com>
2018-02-06 18:41:11 +0800

    demo modify

--- src/bin/demo.rs
+++ src/bin/demo.rs
@@ -1,14 +1,42 @@
+use std::rc::Rc;
 
-use std::collections::HashMap;
-
-struct Cache {
-
+///////////////
+struct Cache<T> {
+    age: T
 }
 
-fn main(){
-    let mut data = HashMap::new();
-    //data.insert("a", 32);
-    data.insert("b", "a string");
+//////////////////
+struct App{
+    cache: Option<Cache<i32>>,
+    //address: Option<Box<String>>,
+    address: Option<String>,
+    age: i32,
+}
 
-    println!("{}", data.get("b").unwrap());
+impl App {
+    fn new() -> App{
+        App{
+            cache: None,
+            address: None,
+            age: 0,
+        }
+    }
+    fn set_address(&mut self, address:String) -> &mut Self{
+        //self.address = Some(Box::new(address))
+        self.address = Some(address);
+        self
+    }
+    fn set_age(&mut self, age: i32) -> &mut Self {
+        self.age = age;
+        self
+    }
+}
+
+
+////////////////////
+fn main(){
+    let mut app = App::new();
+    app.set_address("test1".to_string())
+        .set_age(23);
+    println!("init {} : {}", app.address.unwrap(), app.age);
 }
